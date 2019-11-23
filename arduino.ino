7ea51aecca68888c38840b26c8b4bdbe47a1e536
Neal Gordon <nagordon@zoho.com>
2019-11-22 22:57:51 -0800

    Update arduino.ino

--- arduino.ino
+++ arduino.ino
@@ -10,6 +10,9 @@ block comments
 // define interget variable
 int mypin = 10;
 
+// built in pin is 13
+pinMode(13, OUTPUT);
+
 /*
 Digital Voltage 
 LOW is <3v
@@ -20,4 +23,23 @@ HIGH is >3v
 // PWM/digital pins go from 0-255
 
 // analog pins range from 0-1023
+
+
+
+// to print to monitor
+void setup() {
+  Serial.begin(9600);
+  }
+
+void loop() {
+  Serial.println(currentMillis);
+  }
+
+
+// to print multiple values in a line
+Serial.print("sensor = ");
+Serial.print(sensorValue);
+Serial.print("\t output = ");
+Serial.println(outputValue); // prints and adds carriage return
+Serial.print("\n"); // carriage return
 
